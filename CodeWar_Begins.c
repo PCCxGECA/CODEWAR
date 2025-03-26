7e49a79498bb7e119f9a056963154fde03538056
Abir Gupta <22cs04@ecajmer.ac.in>
2025-03-26 17:58:37 +0530

    Programming and Competitive Coding Club

---
+++ CodeWar_Begins.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void nextHint() {
+    printf("Next: %c%c%c%c%c%c%c\n", 114, 56, 118, 51, 97, 108, 112); 
+}
+
+int main() {
+    int num = 5
+    if (num = 5) { 
+        nextHint();
+    }
+
+    int arr[3] = {1, 2, 3, 4};  
+
+    printf("Debugging Complete!\n");
+    return 0;
+}
